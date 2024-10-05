dac15903e9b4fb524c39f82aad2178d662b785f6
wmiq84 <michaelwang3489@gmail.com>
2024-10-05 16:38:48 -0700

    Isolate pokemon creation to component file

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import PokemonImages from './components/Pokemon.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
 		<App />
+    <PokemonImages />
 	</StrictMode>
 );
-
-for (let i = 1; i <= 12; i++) {
-  const pokemonImage = document.createElement('img');
-
-  fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-    .then((response) => response.json())
-    // .then(allpokemon => console.log(allpokemon))
-    .then((data) => {
-      console.log('Fetched data:', data); // Debugging step
-      const imageUrl = data.sprites.front_default;
-      pokemonImage.setAttribute('src', imageUrl);
-      document.body.appendChild(pokemonImage);
-    });
-}
